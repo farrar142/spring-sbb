951bc256b218ec4dcf633b54078793bf3b3f4811
farrar142 <gksdjf1690@gmail.com>
2024-11-11 13:50:46 +0900

    feature: 답변 수정 기능 추가하기

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
 
@@ -16,12 +18,26 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content,SiteUser siteUser) {
+    public void create(Question question, String content, SiteUser siteUser) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(siteUser);
         this.answerRepository.save(answer);
+    }
+    
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        }
+        throw new DataNotFoundException("answer not found");
+    }
+
+    public Answer modify(Answer answer, String content) {
+        answer.setContent(content);
+        this.answerRepository.save(answer);
+        return answer;
     }
 }
